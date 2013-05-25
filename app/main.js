a5e5d53726a00812a02bf1a16d8ee22a8dd022e0
mrhack <hdg1988@gmail.com>
2013-05-25 16:16:20 +0800

    add pause

--- app/main.js
+++ app/main.js
@@ -552,6 +552,7 @@ define(function(require, exports, module) {
     }
 
     var pause = function(){
+        if( gStatus && gStatus.gameStatus != game.GAME_PLAYING ) return;
         // hide the counter panel
         $counter.hide();
         // stop the counterTimer
