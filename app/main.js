450d5f7a6a9e1a24d72af349aed8b5cab491be61
tonysh518 <tony@fuel-it-up.com>
2013-06-06 17:31:28 +0800

    updated gallery

--- app/main.js
+++ app/main.js
@@ -450,7 +450,7 @@ define(function(require, exports, module) {
         var tpl = '<tr><td>#{i}</td><td>#{n}</td><td>#{t}</td><td>#{d}</td></tr>';
 
         $.each( dataArr , function( i , data ){
-            var item = data.original;
+            var item = data;
             var time = item.time;
             var m = ~~ ( time / 1000 / 60 );
             var s = ~~ ( time / 1000 % 60 );
@@ -828,13 +828,14 @@ define(function(require, exports, module) {
                       },
                       afterShow: function(){
                         var picurl = $(this).attr('href');
+                        var sharecopy = "#911五十周年# 我正在浏览保时捷911五十周年图片，你也来看看吧！";
                         $('.fancybox-download').attr('href',picurl.replace('jpg','zip'));
-                        $('.fancybox-share-list').append('<a target="_blank" href="http://v.t.sina.com.cn/share/share.php?title=911%2050th&amp;pic=http://50years911.porsche-events.cn/'+picurl+'&amp;appkey=2455498088" title="分享到新浪微博" class="sina"></a>' +
-                            '<a target="_blank" href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=http://50years911.porsche-events.cn%2f&amp;amp;title=911%2050th&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到QQ空间" class="qzone"></a>' +
-                            '<a target="_blank" href="http://www.kaixin001.com/repaste/share.php?rtitle=Fascination+Porsche+2013&amp;amp;rurl=http://50years911.porsche-events.cn%2f&amp;amp;rcontent=911%2050th&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到开心网" class="kaixing"></a>' +
-                            '<a target="_blank" href="http://v.t.qq.com/share/share.php?title=911%2050th&amp;&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到QQ微博" class="qqwb"></a>' +
-                            '<a target="_blank" href="http://share.renren.com/share/buttonshare.do?link=http://50years911.porsche-events.cn%2f&amp;title=911%2050th&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到人人网" class="renren"></a>' +
-                            '<a target="_blank" href="http://t.sohu.com/third/post.jsp?&amp;url=http://50years911.porsche-events.cn%2f&amp;title=911%2050th&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到搜狐微博" class="tt"></a>');
+                        $('.fancybox-share-list').append('<a target="_blank" href="http://v.t.sina.com.cn/share/share.php?title="+sharecopy+"&amp;pic=http://50years911.porsche-events.cn/'+picurl+'&amp;appkey=2455498088" title="分享到新浪微博" class="sina"></a>' +
+                            '<a target="_blank" href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=http://50years911.porsche-events.cn%2f&amp;amp;title="+sharecopy+"&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到QQ空间" class="qzone"></a>' +
+                            '<a target="_blank" href="http://www.kaixin001.com/repaste/share.php?rtitle=Fascination+Porsche+2013&amp;amp;rurl=http://50years911.porsche-events.cn%2f&amp;rcontent="+sharecopy+"&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到开心网" class="kaixing"></a>' +
+                            '<a target="_blank" href="http://v.t.qq.com/share/share.php?title="+sharecopy+"&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到QQ微博" class="qqwb"></a>' +
+                            '<a target="_blank" href="http://share.renren.com/share/buttonshare.do?link=http://50years911.porsche-events.cn%2f&amp;title="+sharecopy+"&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到人人网" class="renren"></a>' +
+                            '<a target="_blank" href="http://t.sohu.com/third/post.jsp?&amp;url=http://50years911.porsche-events.cn%2f&amp;title="+sharecopy+"&amp;pic=http://50years911.porsche-events.cn/'+picurl+'" title="分享到搜狐微博" class="tt"></a>');
 
                             if(_isIpad){
                                 $('.fancybox-share-list').appendTo('.fancybox-outer');
@@ -890,6 +891,7 @@ define(function(require, exports, module) {
     var gameOver = function( result , isWin){
         // show spirit panel
         showPanel( panelConfigs['spirit-panel'] , {result:result , isWin:isWin} );
+        $('.follow_sina').fadeIn();
 
         // Save record
         var _time = result.time;
@@ -1184,6 +1186,7 @@ define(function(require, exports, module) {
                 $(this).hide();
                 ready();
             });
+            $('.follow_sina').fadeOut();
 
             // run the robot car
             // setTimeout(runRobot,2000);
